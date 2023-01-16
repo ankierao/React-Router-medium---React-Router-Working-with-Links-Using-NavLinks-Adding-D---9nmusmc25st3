b81a9b21f0c538f340656de322b1315a9aec250b
Ankit Rao <110398130+ankierao@users.noreply.github.com>
2023-01-17 00:26:25 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,42 +1,33 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
+function operatorResult(a, b, op) {
+  switch (op) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    case "%":
+      return a % b;
 
-
-export const Calculator = () => {
-    let url_string = window.location.href;
-    var url = new URL(url_string);
-    var num1 = Number(url.searchParams.get("num1"))
-    var num2 = Number(url.searchParams.get("num2"))
-    var op = url.searchParams.get("op")
-    console.log(num1.num2, op)
-    let ans = 0;
-
-    switch (op) {
-        case "+": {
-            ans = num1 + num2;
-            break;
-        }
-        case "/": {
-            ans = num1 / num2;
-            break;
-        }
-        case "-": {
-            ans = num1 - num2;
-            break;
-        }
-        case "*": {
-            ans = num1 * num2;
-            break;
-        }
-        case "%": {
-            ans = num1 % num2;
-            break;
-        }
-    }
-    return (
-        <div>
-
-            Your calculation Result is <span id="calc-result">{ans}</span>
-
-        </div>
-    )
+    default:
+      return;
+  }
 }
+export const Calculator = () => {
+  const [search, setSearch] = useSearchParams();
+  const num1 = search.get("num1");
+  const operator = search.get("op");
+  const num2 = search.get("num2");
+
+  const result = operatorResult(Number(num1), Number(num2), operator);
+  console.log(num1);
+  return (
+    <div>
+      Your calculation Result is <span id="calc-result">{result}</span>
+    </div>
+  );
+};
