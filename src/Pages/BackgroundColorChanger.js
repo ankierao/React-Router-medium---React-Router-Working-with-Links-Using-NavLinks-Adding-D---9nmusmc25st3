84bd1e6a740b6ae8f51a28ffdaed56365047cb8f
Ankit Rao <110398130+ankierao@users.noreply.github.com>
2023-01-17 00:25:48 +0530

    Update BackgroundColorChanger.js

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const BackgroundColorChanger = () => {
-    let { colorname } = useParams();
-    console.log(colorname);
-    return (
-        <main style={{ height: "100vh", backgroundColor: `${colorname}` }}>
-            Background Color Changer
-        </main>
-    )
-}
+  const [bg, setBg] = useState("");
+  const params = useParams();
+  useEffect(() => {
+    setBg(params.colorname);
+    console.log(bg);
+  }, [bg]);
+  return <main style={{ height: "100vh", backgroundColor: `${bg}` }}></main>;
+};
